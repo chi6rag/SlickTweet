4e8a8f34ddebf397609d0be86fe8c24d569620de
Chirag Aggarwal <me@chi6rag.net>
2015-09-25 18:17:12 +0530

    Adding persistance to tweet

--- lib/slick_tweet/models/tweet.rb
+++ lib/slick_tweet/models/tweet.rb
@@ -1,20 +1,55 @@
 module SlickTweet
-	class Tweet < Record
-		attr_accessor :body
-		attr_reader :created_at, :id, :user_id
+  class Tweet < Record
+    attr_accessor :body
+    attr_reader :created_at, :id, :user_id
 
-		def initialize(id: nil, body: nil, user_id: nil, created_at: nil)
-			@id = id || nil
-			@body = body
-			@user_id = user_id
-			@created_at = created_at || nil
-		end
+    def initialize(id: nil, body: nil, user_id: nil, created_at: nil)
+      @id = id || nil
+      @body = body
+      @user_id = user_id
+      @created_at = created_at || nil
+    end
 
-		# SlickTweet::Tweet.count
-		# returns the number of tweets in the database
-		def self.count
-			($con.exec 'SELECT COUNT(*) FROM tweets').values.flatten[0].to_i
-		end
+    def save
+      # cook statement for sql
+      # execute statement
+        # if successful, find saved object and return
+        # - else
+          # - do not save the object
+          # - add errors
+          # - return unsaved object with errors
+      statement = 'INSERT INTO tweets(body, user_id) '
+      statement << "VALUES('#{body}', '#{user_id}') "
+      statement << "RETURNING id, body, user_id, created_at"
+      begin
+        res = $con.exec(statement).values.flatten
+      rescue PG::Error => e
+        puts e.message
+        puts ''
+        return nil
+      end
+      psql_to_tweet(res)
+    end
 
-	end
+    # SlickTweet::Tweet.count
+    # returns the number of tweets in the database
+    def self.count
+      ($con.exec 'SELECT COUNT(*) FROM tweets').values.flatten[0].to_i
+    end
+
+    private
+    
+    # psql_to_tweet
+    # postgres returns data in array
+    # with all data types as strings
+    # psql_to_tweet converts psql result to Tweet object
+    def psql_to_tweet(psql_result)
+      id         = psql_result[0].to_i
+      body       = psql_result[1]
+      user_id    = psql_result[2].to_i
+      created_at = DateTime.new(*(psql_result[3].split(/[-|\s|:]/).map{|param| param.to_f}))
+      Tweet.new(id: id, body: body, user_id: user_id, created_at: created_at)
+    end
+
+  end
 end
--- spec/slick_tweet/models/tweet_spec.rb
+++ spec/slick_tweet/models/tweet_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe SlickTweet::Tweet do
   before(:all){
     # establish pg connection
     $con = PG.connect :dbname => 'slick_tweet_testing'
-  	# create a test user
-  	@user = SlickTweet::User.create(
+    # create a test user
+    @user = SlickTweet::User.create(
       username: 'test123',
       email: 'test123@example.com',
       password: '!Abcd1234'
@@ -16,32 +16,32 @@ RSpec.describe SlickTweet::Tweet do
   }
 
   after(:all){
-    $con.exec "DELETE FROM users"
     $con.exec "DELETE FROM tweets"
+    $con.exec "DELETE FROM users"
     $con.close if $con
   }
 
   describe '#new' do
-		it 'creates a new but unsaved tweet' do 
-			count_before = SlickTweet::Tweet.count
-			tweet = SlickTweet::Tweet.new(
-								body: 'Testing Stuff',
-								user_id: @user.id
-							)
-			count_after = SlickTweet::Tweet.count
-			expect(count_before).to eq count_after
-		end
+    it 'creates a new but unsaved tweet' do 
+      count_before = SlickTweet::Tweet.count
+      tweet = SlickTweet::Tweet.new(
+                body: 'Testing Stuff',
+                user_id: @user.id
+              )
+      count_after = SlickTweet::Tweet.count
+      expect(count_before).to eq count_after
+    end
   end
 
   describe '#count' do
     it 'returns the number of tweets' do
-    	expect(SlickTweet::Tweet.count).to eq(0)
-    	tweet = SlickTweet::Tweet.new(
-								body: 'Testing Stuff',
-								user_id: @user.id
-							)
-    	tweet.save
-    	expect(Tweet.count).to eq(1)
+      expect(SlickTweet::Tweet.count).to eq(0)
+      tweet = SlickTweet::Tweet.new(
+                body: 'Testing Stuff',
+                user_id: @user.id
+              )
+      tweet.save
+      expect(SlickTweet::Tweet.count).to eq(1)
     end
   end
 
