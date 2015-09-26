739dc10cf0a275e73d70a1037574a94b245bcabc
Chirag Aggarwal <me@chi6rag.net>
2015-09-26 16:42:56 +0530

    slick_tweet_spec | Adding spec for handling invalid tweets                  | Refactor RSpec code

--- spec/slick_tweet/models/tweet_spec.rb
+++ spec/slick_tweet/models/tweet_spec.rb
@@ -4,7 +4,7 @@ require 'slick_tweet'
 require "rspec/expectations"
 
 RSpec.describe SlickTweet::Tweet do
-  before(:all){
+  before(:all) do
     # establish pg connection
     $con = PG.connect :dbname => 'slick_tweet_testing'
     # create a test user
@@ -13,17 +13,15 @@ RSpec.describe SlickTweet::Tweet do
       email: 'test123@example.com',
       password: '!Abcd1234'
     )
-  }
+  end
 
-  after(:each){
-    $con.exec "DELETE FROM tweets"
-  }
+  after(:each){ $con.exec "DELETE FROM tweets" }
 
-  after(:all){
+  after(:all) do
     $con.exec "DELETE FROM tweets"
     $con.exec "DELETE FROM users"
     $con.close if $con
-  }
+  end
 
   describe '.new' do
     it 'creates a new but unsaved tweet' do 
@@ -39,17 +37,30 @@ RSpec.describe SlickTweet::Tweet do
   end
 
   describe '#save' do
+    before(:each) { @count_before = SlickTweet::Tweet.count }
+
+    after(:each){ $con.exec "DELETE FROM tweets" }
+
     context 'with valid attributes' do
       it 'saves the tweet in the database' do
-        count_before = SlickTweet::Tweet.count
+        expect{
+          tweet = SlickTweet::Tweet.new(
+                    body: 'Testing Stuff',
+                    user_id: @user.id
+                  )
+          tweet.save
+          }.to change{SlickTweet::Tweet.count}.by 1
+      end
+    end
+    
+    context 'with invalid attributes' do
+      it "does not save empty tweets in database" do
         tweet = SlickTweet::Tweet.new(
-                  body: 'Testing Stuff',
+                  body: '',
                   user_id: @user.id
                 )
         tweet.save
-        count_after = SlickTweet::Tweet.count
-        expect(count_before).to_not eq(count_after)
-        expect(count_after).to eq(1)
+        expect(@count_before).to eq(SlickTweet::Tweet.count)
       end
     end
   end
