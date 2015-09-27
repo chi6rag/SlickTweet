edc877056b2c634051501275db07ad8783d9ec77
Chirag Aggarwal <me@chi6rag.net>
2015-09-27 17:27:23 +0530

    Adding logout method to sessions

--- lib/slick_tweet/views/sessions_view.rb
+++ lib/slick_tweet/views/sessions_view.rb
@@ -1,63 +1,68 @@
 module SlickTweet
-	
-	class SessionsView < View
 
-		# controller level logic
-		# to be tested
-		def login
-			unless $current_user
-				user_details = details_from_login
-				system "clear"
-				user = SlickTweet::User.find(user_details)
-				unless user
-					puts "Wrong Email or Password Combination! Try again"
-					return
-				end
-				$current_user = user
-			end
-			SlickTweet.current_screen = 'welcome'
-			$current_user
-		end
+  class SessionsView < View
 
-		# to be tested
-		def sign_up
-			unless $current_user
-				user_details = details_from_signup
-				system "clear"
-				user = SlickTweet::User.create(user_details)
-				if user
-					$current_user = user
-					SlickTweet.current_screen = 'welcome'
-				else
-					$current_user = nil
-				end
-				$current_user
-			end
-		end
-		
-		# view
-		def details_from_login(clear: false)
-			puts "\n\nSlickTweet Login\n"
-			puts "----------------\n"
-			print "Username / Email: "
-			username_or_email = gets.chomp
-			print "Password: "
-			password = gets.chomp
-			{username_or_email: username_or_email, password: password}
-		end
-		
-		def details_from_signup
-			puts "\n\nSlickTweet Signup\n"
-			puts "----------------\n"
-			print "Username: "
-			username = gets.chomp
-			print "Email: "
-			email = gets.chomp
-			print "Password: "
-			password = gets.chomp
-			{username: username, email: email, password: password}
-		end
+    # controller level logic
+    # to be tested
+    def login
+      unless $current_user
+        user_details = details_from_login
+        system "clear"
+        user = SlickTweet::User.find(user_details)
+        unless user
+          puts "Wrong Email or Password Combination! Try again"
+          return
+        end
+        $current_user = user
+      end
+      SlickTweet.current_screen = 'welcome'
+      $current_user
+    end
 
-	end
+    # to be tested
+    def sign_up
+      unless $current_user
+        user_details = details_from_signup
+        system "clear"
+        user = SlickTweet::User.create(user_details)
+        if user
+          $current_user = user
+          SlickTweet.current_screen = 'welcome'
+        else
+          $current_user = nil
+        end
+        $current_user
+      end
+    end
+
+    def logout
+      $current_user = nil
+      SlickTweet::current_screen  = 'home'
+    end
+
+    # view
+    def details_from_login(clear: false)
+      puts "\n\nSlickTweet Login\n"
+      puts "----------------\n"
+      print "Username / Email: "
+      username_or_email = gets.chomp
+      print "Password: "
+      password = gets.chomp
+      {username_or_email: username_or_email, password: password}
+    end
+
+    def details_from_signup
+      puts "\n\nSlickTweet Signup\n"
+      puts "----------------\n"
+      print "Username: "
+      username = gets.chomp
+      print "Email: "
+      email = gets.chomp
+      print "Password: "
+      password = gets.chomp
+      {username: username, email: email, password: password}
+    end
+
+  end
 
 end
--- spec/slick_tweet/views/sessions_view_spec.rb
+++ spec/slick_tweet/views/sessions_view_spec.rb
@@ -3,27 +3,44 @@ require 'slick_tweet'
 require "rspec/expectations"
 
 RSpec.describe SlickTweet::SessionsView do
-	before(:all){
-		@sessions_view = SlickTweet::SessionsView.new
-	}
+  before(:all){
+    @sessions_view = SlickTweet::SessionsView.new
+  }
 
-	describe '#details_from_login' do
-		it "gives a hash of username_or_email and password" do
-			allow(@sessions_view).to receive(:gets).and_return('chi6rag', '1234567890')
-			
-			expect(@sessions_view.details_from_login)
-			.to eq({username_or_email: 'chi6rag', password: '1234567890'})
-		end
-	end
+  describe '#logout' do
+    before(:each) do
+      $current_user = double(:user)
+      SlickTweet::current_screen = 'welcome'
+    end
 
-	describe '#details_from_signup' do
-		it 'gives a hash of username, email and password' do 
-			allow(@sessions_view).to receive(:gets)
-			.and_return('chi6rag', 'me@chi6rag.net', '1234567890')
+    it 'changes current screen to home' do
+      @sessions_view.logout
+      expect(SlickTweet::current_screen).to eq('home')
+    end
 
-			expect(@sessions_view.details_from_signup)
-			.to eq({username: 'chi6rag', email: 'me@chi6rag.net', password: '1234567890'})
-		end
-	end
+    it 'sets current_user to nil' do 
+      @sessions_view.logout
+      expect($current_user).to be_nil
+    end
+  end
+
+  describe '#details_from_login' do
+    it "gives a hash of username_or_email and password" do
+      allow(@sessions_view).to receive(:gets).and_return('chi6rag', '1234567890')
+
+      expect(@sessions_view.details_from_login)
+      .to eq({username_or_email: 'chi6rag', password: '1234567890'})
+    end
+  end
+
+  describe '#details_from_signup' do
+    it 'gives a hash of username, email and password' do
+      allow(@sessions_view).to receive(:gets)
+      .and_return('chi6rag', 'me@chi6rag.net', '1234567890')
+
+      expect(@sessions_view.details_from_signup)
+      .to eq({username: 'chi6rag', email: 'me@chi6rag.net', password: '1234567890'})
+    end
+  end
 
 end
