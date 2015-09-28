6d1e4c4f3f14f00d0cfd7a388d1d3157005e3fdd
Chirag Aggarwal <me@chi6rag.net>
2015-09-28 14:59:06 +0530

    home_screen_view      | Adding timeline option home_screen_view_spec | Test presence of timeline option

--- lib/slick_tweet/views/home_screen_view.rb
+++ lib/slick_tweet/views/home_screen_view.rb
@@ -1,47 +1,51 @@
 module SlickTweet
 
-	class HomeScreenView < View
-		
-		# controller
-		
-		# render
-		# test this method using an integration test suite
-		def render
-			print options
-			choice = gets.chomp
-			handle_choice choice
-		end
+  class HomeScreenView < View
+    
+    # controller
+    
+    # render
+    # test this method using an integration test suite
+    def render
+      print options
+      choice = gets.chomp
+      handle_choice choice
+    end
 
     private
     
-		def handle_choice choice
-			case choice
-			when "1"
-				# render sign up view
-				SlickTweet.current_screen = 'sign_up'
-			when "2"
-				# render login view
-				SlickTweet.current_screen = 'login'
-			when "3"
-				SlickTweet.current_screen = 'exit'
-			else
-				puts 'Wrong choice'
-				choice = gets.chomp
-				handle_choice choice
-			end
-		end
+    def handle_choice choice
+      case choice
+      when "1"
+        # render sign up view
+        SlickTweet.current_screen = 'sign_up'
+      when "2"
+        # render login view
+        SlickTweet.current_screen = 'login'
+      when "3"
+        SlickTweet.current_screen = 'timeline'
+      when "4"
+        SlickTweet.current_screen = 'exit'
+      else
+        print "\nWrong choice\n"
+        print 'Choose: '
+        choice = gets.chomp
+        handle_choice choice
+      end
+    end
     
-		# view
-		def options
-			options = "Welcome to SlickTweet\n"
-			options << "--------------------\n"
-			options << "1. Sign Up\n"
-			options << "2. Login\n"
-			options << "3. Exit\n"
-			options << "Choose: "
-			options
-		end
+    # view
+    def options
+      options = "Welcome to SlickTweet\n"
+      options << "--------------------\n"
+      options << "1. Sign Up\n"
+      options << "2. Login\n"
+      options << "3. View Timeline\n"
+      options << "4. Exit\n"
+      options << "Choose: "
+      options
+    end
 
-	end
+  end
 
 end
--- spec/slick_tweet/views/home_screen_view_spec.rb
+++ spec/slick_tweet/views/home_screen_view_spec.rb
@@ -3,33 +3,34 @@ require 'slick_tweet'
 require "rspec/expectations"
 
 RSpec.describe SlickTweet::HomeScreenView do
-	before(:all){
-		@home_screen_view = SlickTweet::HomeScreenView.new
-	}
+  before(:all){
+    @home_screen_view = SlickTweet::HomeScreenView.new
+  }
 
-	describe '#options' do
-		it "returns the correct options" do 
-			options = "Welcome to SlickTweet\n"
-			options << "--------------------\n"
-			options << "1. Sign Up\n"
-			options << "2. Login\n"
-			options << "3. Exit\n"
-			options << "Choose: "
-			options
-			expect(@home_screen_view.send(:options)).to eq(options)
-		end
-	end
+  describe '#options' do
+    it "returns the correct options" do 
+      options = "Welcome to SlickTweet\n"
+      options << "--------------------\n"
+      options << "1. Sign Up\n"
+      options << "2. Login\n"
+      options << "3. View Timeline\n"
+      options << "4. Exit\n"
+      options << "Choose: "
+      options
+      expect(@home_screen_view.send(:options)).to eq(options)
+    end
+  end
 
-	describe '#handle_choice' do
-		before(:each){ SlickTweet::current_screen = 'home'}
+  describe '#handle_choice' do
+    before(:each){ SlickTweet::current_screen = 'home'}
 
-    {'sign_up': "1", 'login': "2", 'exit': "3"}.each do |screen_name, choice|
+    {'sign_up': "1", 'login': "2", 'timeline': '3', 'exit': "4"}.each do |screen_name, choice|
       it "handles choice for #{screen_name}" do
         expect( @home_screen_view.send(:handle_choice, choice) ).to eq(SlickTweet::current_screen)
         expect(SlickTweet::current_screen).to eq(screen_name.to_s)
       end
     end
 
-	end
+  end
 
 end
