b3ba31ecd2cb759e247b4801795d9d63d79be0d0
Chirag Aggarwal <me@chi6rag.net>
2015-10-04 17:02:58 +0530

    Adding conditional for preSignInChoice

--- src/main/java/Cli.java
+++ src/main/java/Cli.java
@@ -1,12 +1,27 @@
+import org.apache.commons.configuration.ConfigurationException;
 import java.util.Scanner;
 
 public class Cli {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ConfigurationException {
         Scanner scanner = new Scanner(System.in);
         Authentication authentication = new Authentication();
 
         System.out.print(authentication.getPreSignInOptions());
         int preSignInChoice = scanner.nextInt();
+        switch(preSignInChoice){
+            case 1:
+                System.out.println("Implement Sign Up");
+                break;
+            case 2:
+                System.out.println("Implement Login");
+                break;
+            case 3:
+                System.out.println("Implement Exit");
+                break;
+            default:
+                System.out.println("Unrecognized Option");
+                break;
+        }
 
     }
 }
