55581768282f8820d550494f791137dfa1858bf5
Chirag Aggarwal <me@chi6rag.net>
2015-10-04 20:41:09 +0530

    Adding code for getting sign up details from user

--- src/main/java/Authentication.java
+++ src/main/java/Authentication.java
@@ -1,4 +1,5 @@
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class Authentication {
 
@@ -11,6 +12,18 @@ public class Authentication {
                 "3. Exit\n"         +
                 "Choose: ";
         return preSignInOptions;
+    }
+
+    public Hashtable getSignUpDetails(){
+        Scanner consoleInput = new Scanner(System.in);
+        System.out.println("Username: ");
+        String username = consoleInput.nextLine();
+        System.out.println("Password: ");
+        String password = consoleInput.nextLine();
+        Hashtable signUpDetails = new Hashtable();
+        signUpDetails.put("username", username);
+        signUpDetails.put("password", password);
+        return signUpDetails;
     }
 
 }
