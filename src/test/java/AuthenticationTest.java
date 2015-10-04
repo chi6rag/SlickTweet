f229968d2ea9a75cfcc527baef28d8aea886eca8
Chirag Aggarwal <me@chi6rag.net>
2015-10-04 20:39:43 +0530

    Adding test for getting sign up details from user

--- src/test/java/AuthenticationTest.java
+++ src/test/java/AuthenticationTest.java
@@ -1,11 +1,15 @@
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.Hashtable;
 
 public class AuthenticationTest {
     Authentication auth = new Authentication();
 
     @Test
-    public void testGetsPreSignInOptions(){
+    public void testGetsPreSignInOptions() {
         String preSignInOptions =
                 "  TwitchBlade  \n" +
                 "---------------\n" +
@@ -14,6 +18,26 @@ public class AuthenticationTest {
                 "3. Exit\n"         +
                 "Choose: ";
         assertEquals(auth.getPreSignInOptions(), preSignInOptions);
+    }
+
+//  to-do
+//  assert stdout to contain what has been asked
+//    - username
+//    - password
+    @Test
+    public void testGetsSignUpDetails(){
+//      ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
+//      System.setOut(new PrintStream(consoleOutput));
+        ByteArrayInputStream consoleInput = new
+                ByteArrayInputStream("foo_example\n123456789".getBytes());
+        System.setIn(consoleInput);
+
+        Hashtable signUpDetails = new Hashtable();
+        signUpDetails.put("username", "foo_example");
+        signUpDetails.put("password", "123456789");
+
+        assertEquals(auth.getSignUpDetails(), signUpDetails);
+        System.setIn(System.in);
     }
 
 }
