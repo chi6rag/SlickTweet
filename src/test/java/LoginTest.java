9c0c924c20e4aff42dcee349e1152d0fd8b6bd8a
Chirag Aggarwal <me@chi6rag.net>
2015-10-05 12:21:19 +0530

    LoginTest | Adding stub for auth.getAuthDetails method           | Adding before hook to add a user to testing database           | Adding after hook to delete all users from Testing DB

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertEquals;
 public class LoginTest {
     Authentication auth = new Authentication();
     Boolean isConnectionSetup = false;
-    PreparedStatement deleteUsersStatement;
+    PreparedStatement preparedStatement;
     Connection connection;
     Hashtable authDetails;
 
@@ -41,6 +41,42 @@ public class LoginTest {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    @Before
+    public void initializeBeforeEach(){
+
+        // Setup stub for auth.getAuthDetails()
+        authDetails = new Hashtable();
+        authDetails.put("username", "foo_example");
+        authDetails.put("password", "123456789");
+        Mockito.when(auth.getAuthDetails()).thenReturn(authDetails);
+
+        // Add user to twitchblade_testing
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO users(username, password) " +
+                    "VALUES(?, ?)"
+            );
+            preparedStatement.setString(1, "foo_example");
+            preparedStatement.setString(2, "123456789");
+            preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            System.out.println("Unable to setup Test Data for User");
+            e.printStackTrace();
+        }
+
+    }
+
+    @After
+    public void deleteAllUsers(){
+        // Delete All Users
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM users");
+            preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
