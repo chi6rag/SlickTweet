e39b664144eda032fc25a21ddacca50b4d4a5bf3
Chirag Aggarwal <me@chi6rag.net>
2015-10-05 14:19:00 +0530

    LoginTest | Refactoring Test Setup

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,11 @@
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.Mockito;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Hashtable;
-
-import static java.sql.DriverManager.getConnection;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -19,12 +14,47 @@ import static org.junit.Assert.assertEquals;
 public class LoginTest {
     Authentication auth = new Authentication();
     Boolean isConnectionSetup = false;
-    PreparedStatement preparedStatement;
-    Connection connection;
+    PreparedStatement preparedStatement = null;
+    Connection connection = null;
     Hashtable authDetails;
 
     @Before
-    public void initializeDBConnectionBeforeAll(){
+    public void beforeEach(){
+        initializeDBConnection();
+
+        // Setup authDetails
+        authDetails = new Hashtable();
+        authDetails.put("username", "foo_example");
+        authDetails.put("password", "123456789");
+
+        // Add user to twitchblade_testing
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO users(username, password) " +
+                    "VALUES(?, ?)"
+            );
+            preparedStatement.setString(1, "foo_example");
+            preparedStatement.setString(2, "123456789");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            System.out.println("------ Unable to setup Test Data for User ------");
+            e.printStackTrace();
+        }
+
+    }
+
+    @After
+    public void afterEach(){
+        // Delete All Users
+        try {
+            preparedStatement = connection.prepareStatement("DELETE FROM users");
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeDBConnection(){
         if(!isConnectionSetup){
             try {
                 Class.forName("org.postgresql.Driver");
@@ -41,42 +71,6 @@ public class LoginTest {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    @Before
-    public void initializeBeforeEach(){
-
-        // Setup stub for auth.getAuthDetails()
-        authDetails = new Hashtable();
-        authDetails.put("username", "foo_example");
-        authDetails.put("password", "123456789");
-        Mockito.when(auth.getAuthDetails()).thenReturn(authDetails);
-
-        // Add user to twitchblade_testing
-        try {
-            preparedStatement = connection.prepareStatement(
-                    "INSERT INTO users(username, password) " +
-                    "VALUES(?, ?)"
-            );
-            preparedStatement.setString(1, "foo_example");
-            preparedStatement.setString(2, "123456789");
-            preparedStatement.executeQuery();
-        } catch (SQLException e) {
-            System.out.println("Unable to setup Test Data for User");
-            e.printStackTrace();
-        }
-
-    }
-
-    @After
-    public void deleteAllUsers(){
-        // Delete All Users
-        try {
-            preparedStatement = connection.prepareStatement("DELETE FROM users");
-            preparedStatement.executeQuery();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 
