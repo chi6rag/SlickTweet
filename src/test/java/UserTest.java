e726261de45e7719e008c66b202dd7f64ca00624
Chirag Aggarwal <me@chi6rag.net>
2015-10-05 20:06:50 +0530

    UserTest | Testing #save method when called on valid user returns saved user          | Testing #save method when called on invalid user keeps user count same          | Testing #save with invalid user object returns null

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -2,7 +2,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import java.sql.*;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertSame;
 
 public class UserTest {
     PreparedStatement preparedStatement = null;
@@ -28,13 +31,28 @@ public class UserTest {
     }
 
     @Test
-    public void testSaveWithValidUserObjectReturnsTheSavedUser(){}
+    public void testSaveWithValidUserObjectReturnsTheSavedUser(){
+        User user = new User("foo_example", "123456789");
+        User savedUser = user.save();
+        assertNotEquals(savedUser.getUsername(), "foo_example");
+        assertNotEquals(savedUser.getPassword(), "123456789");
+        assertNotEquals(savedUser.getClass(), User);
+    }
 
     @Test
-    public void testSaveWithInvalidUserObjectKeepsUserCountSame(){}
+    public void testSaveWithInvalidUserObjectKeepsUserCountSame(){
+        int beforeCount = getUserCount();
+        User user = new User("ab", "123456789");
+        user.save();
+        int afterCount = getUserCount();
+        assertEquals(beforeCount, afterCount);
+    }
 
     @Test
-    public void testSaveWithInvalidUserObjectReturnsNull(){}
+    public void testSaveWithInvalidUserObjectReturnsNull(){
+        User user = new User("ab", "123456789");
+        assertEquals(user.save(), null);
+    }
 
     private void initializeDBConnection(){
         if(this.connection == null){
