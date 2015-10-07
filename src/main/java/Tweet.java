69b4281092af322468ac3f57a943cefb5e0230b0
Chirag Aggarwal <me@chi6rag.net>
2015-10-07 19:25:11 +0530

    Tweet: Adding Tweet constructor

---
+++ src/main/java/Tweet.java
@@ -0,0 +1,11 @@
+public class Tweet {
+    private Integer id;
+    private String body;
+    private Integer userId;
+
+    Tweet(String body, Integer userId){
+        this.body = body;
+        this.userId = userId;
+    }
+
+}
