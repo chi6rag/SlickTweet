bb0ff188e714f934a96ac1f3040b7c1e8c19d8fb
Chirag Aggarwal <me@chi6rag.net>
2015-10-08 16:29:10 +0530

    Timeline.java: Adding Timeline code

---
+++ src/main/java/Timeline.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class Timeline {
+
+    private DbConnection connection;
+    private User user;
+    private Tweets tweets;
+
+    Timeline(User user, DbConnection connection){
+        this.user = user;
+        this.connection = connection;
+        this.tweets = new Tweets(connection);
+    }
+
+    public ArrayList<Tweet> getTweets(){
+        Hashtable queryHash = new Hashtable();
+        queryHash.put("userId", this.user.getId());
+        return tweets.where(queryHash);
+    }
+
+}
