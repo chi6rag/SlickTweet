48015a6cc06529dc7a2811aa46c41a89b9812453
Chirag Aggarwal <me@chi6rag.net>
2015-10-08 16:29:59 +0530

    TweetTest.java: Adding test to get tweets for a valid user_id and                 return user's tweets

--- src/test/java/TimelineTest.java
+++ src/test/java/TimelineTest.java
@@ -1,7 +1,10 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
 public class TimelineTest {
     DbConnection connection = new DbConnection();
@@ -13,7 +16,7 @@ public class TimelineTest {
     public void BeforeEach(){
         currentUser = generateUser("foo_example", "123456789",
                 this.connection);
-        timeline = new Timeline(currentUser);
+        timeline = new Timeline(currentUser, this.connection);
     }
 
     @After
@@ -22,9 +25,23 @@ public class TimelineTest {
         deleteAllUsers();
     }
 
+    @Test
+    public void testGetTweetsForValidUserIdReturnsUsersTweets(){
+        generateTweet("hello", currentUser.getId(), this.connection);
+        ArrayList<Tweet> tweets = timeline.getTweets();
+        for(int i=0; i<tweets.size(); i++){
+            assertEquals( (tweets.get(i)).getUserId(), currentUser.getId() );
+        }
+    }
+
     private User generateUser(String username, String password,
                               DbConnection connection){
         return (new User(username, password, connection)).save();
+    }
+
+    private Tweet generateTweet(String body, Integer userId,
+                                DbConnection connection){
+        return (new Tweet(body, userId, connection)).save();
     }
 
     private void deleteAllTweets(){
