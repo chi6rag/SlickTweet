a78e881fb518628ad02575e8025843fe28229bf7
Chirag Aggarwal <me@chi6rag.net>
2015-10-12 17:24:41 +0530

    UsersTest: Adding test to find a user by username

--- src/test/java/net/chi6rag/twitchblade_test/UsersTest.java
+++ src/test/java/net/chi6rag/twitchblade_test/UsersTest.java
@@ -3,10 +3,11 @@ package net.chi6rag.twitchblade_test;
 import net.chi6rag.twitchblade.*;
 import test_helpers.*;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import java.util.Hashtable;
+
+import static org.junit.Assert.assertEquals;
 
 public class UsersTest {
     DbConnection connection = new DbConnection();
@@ -30,11 +31,11 @@ public class UsersTest {
         Hashtable validAuthDetails = userTestHelper
                 .getUserDetails("foo_example", "123456789");
         User foundUser = allUsers.find(validAuthDetails);
-        Assert.assertEquals(foundUser.getId().getClass()
+        assertEquals(foundUser.getId().getClass()
                 .getSimpleName(), "Integer");
-        Assert.assertEquals(foundUser.getClass().getSimpleName(), "User");
-        Assert.assertEquals(foundUser.getUsername(), "foo_example");
-        Assert.assertEquals(foundUser.getPassword(), "123456789");
+        assertEquals(foundUser.getClass().getSimpleName(), "User");
+        assertEquals(foundUser.getUsername(), "foo_example");
+        assertEquals(foundUser.getPassword(), "123456789");
     }
 
     @Test
@@ -42,14 +43,23 @@ public class UsersTest {
         Hashtable inexistentAuthDetails = userTestHelper
                 .getUserDetails("baz_example", "123456789");
         User foundUser = allUsers.find(inexistentAuthDetails);
-        Assert.assertEquals(foundUser, null);
+        assertEquals(foundUser, null);
     }
 
     @Test
     public void returnsNullWhenCalledWithWrongUserDetailsHash(){
         Hashtable authDetails = getWrongUserDetailsHash();
         User foundUser = allUsers.find(authDetails);
-        Assert.assertEquals(foundUser, null);
+        assertEquals(foundUser, null);
+    }
+
+    @Test
+    public void testFindByUsernameForValidUsernameReturnsUser(){
+        User foundUser = allUsers.findByUsername("foo_example");
+        assertEquals(foundUser.getUsername(), "foo_example");
+        assertEquals(foundUser.getId().getClass()
+                .getSimpleName(), "Integer");
+        assertEquals(foundUser.getPassword(), "123456789");
     }
 
     private Hashtable getWrongUserDetailsHash(){
