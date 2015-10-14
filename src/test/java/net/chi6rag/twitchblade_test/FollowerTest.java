e11e84421bdf10f5cf1c78e2c9e732dbb078075b
Chirag Aggarwal <me@chi6rag.net>
2015-10-14 10:47:32 +0530

    FollowerTest: Adding test to expect #follow for unsaved user with valid username argument to return null

--- src/test/java/net/chi6rag/twitchblade_test/FollowerTest.java
+++ src/test/java/net/chi6rag/twitchblade_test/FollowerTest.java
@@ -3,12 +3,15 @@ package net.chi6rag.twitchblade_test;
 import net.chi6rag.twitchblade.*;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import test_helpers.RelationshipTestHelper;
 import test_helpers.UserTestHelper;
+import static junit.framework.TestCase.assertNull;
 
 public class FollowerTest {
     DbConnection connection = new DbConnection();
     Follower follower;
+    User user;
 
     // Objects of helper classes
     UserTestHelper userTestHelper = new UserTestHelper(connection);
@@ -17,15 +20,24 @@ public class FollowerTest {
 
     @Before
     public void beforeEach(){
-        User currentUser = userTestHelper.getSavedUserObject("foo_example",
+        user = userTestHelper.getSavedUserObject("foo_example",
                 "123456789", connection);
-        follower = new Follower(currentUser);
+        follower = new Follower(user);
     }
 
     @After
     public void afterEach(){
         relationshipTestHelper.deleteAllRelationships();
         userTestHelper.deleteAllUsers();
+    }
+
+    @Test
+    public void testFollowForUnsavedUserWithValidArgumentsReturnsNull(){
+        User userToFollow = userTestHelper.getSavedUserObject("bar_example",
+                "123456789", this.connection);
+        User followedUser = follower.follow("bar_example");
+        relationshipTestHelper.validateNonFollower(user, followedUser);
+        assertNull(followedUser);
     }
 
 }
