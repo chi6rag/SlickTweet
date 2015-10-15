cb4dbe4c1b277232ddfaaaaaa4d3ef149b7859fc
Chirag Aggarwal <me@chi6rag.net>
2015-10-15 18:27:46 +0530

    ProfileTest: Adding test to expect #getTweets for valid user to return users tweets Adding test to expect #getTweets to return blank arraylist for valid user with no tweets Adding test to expect #getTweets to return null for unsaved user

--- src/test/java/net/chi6rag/twitchblade_test/ProfileTest.java
+++ src/test/java/net/chi6rag/twitchblade_test/ProfileTest.java
@@ -1,34 +1,28 @@
 package net.chi6rag.twitchblade_test;
 
-import net.chi6rag.twitchblade.DbConnection;
-import net.chi6rag.twitchblade.Timeline;
-import net.chi6rag.twitchblade.User;
+import net.chi6rag.twitchblade.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import test_helpers.AssertionTestHelper;
-import test_helpers.RelationshipTestHelper;
 import test_helpers.TweetTestHelper;
 import test_helpers.UserTestHelper;
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
 
 public class ProfileTest {
     DbConnection connection;
     Profile profile;
-    User currentUser;
+    User user;
 
     // Objects of helper classes
     UserTestHelper userTestHelper;
     TweetTestHelper tweetTestHelper;
-    AssertionTestHelper assertionTestHelper;
-    RelationshipTestHelper relationshipTestHelper;
 
     @Before
     public void BeforeEach(){
         connection = new DbConnection();
         userTestHelper = new UserTestHelper(connection);
         tweetTestHelper = new TweetTestHelper(connection);
-        relationshipTestHelper = new RelationshipTestHelper(connection);
-        assertionTestHelper = new AssertionTestHelper();
         user = userTestHelper.getSavedUserObject("foo_example", "123456789",
                 this.connection);
         profile = new Profile(user, this.connection);
@@ -37,12 +31,32 @@ public class ProfileTest {
     @After
     public void afterEach(){
         tweetTestHelper.deleteAllTweets();
-        relationshipTestHelper.deleteAllRelationships();
         userTestHelper.deleteAllUsers();
         connection.close();
     }
 
     @Test
-    public void testGetTweetsFor
+    public void testGetTweetsForValidUserReturnsUsersTweets(){
+        tweetTestHelper.createSampleTweetsFor(user, "hello!", "hello world!");
+        ArrayList<Tweet> tweets = profile.getTweets();
+        for(int i=0; i<tweets.size(); i++){
+            assertEquals(tweets.get(i).getUserId(), user.getId());
+        }
+    }
+
+    @Test
+    public void testGetTweetsReturnsBlankArrayListForValidUserWithNoTweets(){
+        ArrayList<Tweet> tweets = profile.getTweets();
+        assertEquals(tweets.size(), 0);
+    }
+
+    @Test
+    public void testGetTweetsReturnsNullForUnsavedUser(){
+        User unsavedUser = new User("baz_example", "123456789",
+                this.connection);
+        profile = new Profile(unsavedUser, this.connection);
+        ArrayList<Tweet> tweets = profile.getTweets();
+        assertEquals(tweets, null);
+    }
 
 }
