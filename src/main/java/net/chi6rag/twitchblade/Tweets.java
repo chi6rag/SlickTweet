53c5a1efb98754e1011b51871b15148e74580f3c
Chirag Aggarwal <me@chi6rag.net>
2015-10-15 11:16:13 +0530

    Tweets: Adding #forTimelineOf to search all the tweets for the timeline of a particular user

--- src/main/java/net/chi6rag/twitchblade/Tweets.java
+++ src/main/java/net/chi6rag/twitchblade/Tweets.java
@@ -9,6 +9,7 @@ import java.util.Hashtable;
 public class Tweets {
     private final DbConnection connection;
     private PreparedStatement tweetsWherePreparedStatement;
+    private PreparedStatement tweetsForTimelinePreparedStatement;
 
     public Tweets(DbConnection connection){
         this.connection = connection;
@@ -23,6 +24,27 @@ public class Tweets {
         return tweets;
     }
 
+    public ArrayList<Tweet> forTimelineOf(Integer userId){
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+        prepareTweetsForTimelineStatement();
+        ResultSet res = queryTweetsForTimelineFromDb(userId);
+        if(res != null) tweets = getTweetsFromDbResult(res);
+        return tweets;
+    }
+
+    private ResultSet queryTweetsForTimelineFromDb(Integer userId) {
+        if(userId < 0) return null;
+        ResultSet res = null;
+        try {
+            tweetsForTimelinePreparedStatement.setInt(1, userId);
+            tweetsForTimelinePreparedStatement.setInt(2, userId);
+            res = this.tweetsForTimelinePreparedStatement.executeQuery();
+        } catch (SQLException e) {
+            // e.printStackTrace();
+        }
+        return res;
+    }
+
     private void prepareTweetsWhereStatement(){
         if(this.tweetsWherePreparedStatement == null){
             try {
@@ -30,6 +52,23 @@ public class Tweets {
                         .prepareStatement("SELECT * FROM tweets WHERE user_id=?");
             } catch (SQLException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    private void prepareTweetsForTimelineStatement(){
+        if(this.tweetsForTimelinePreparedStatement == null){
+            try {
+                this.tweetsForTimelinePreparedStatement = this.connection
+                    .prepareStatement(
+                            "(SELECT T.id, T.body, T.user_id, T.created_at" +
+                                    " FROM relationship AS R INNER JOIN tweets AS T" +
+                                    " ON R.followed_id=T.user_id WHERE R.follower_id=?)" +
+                                    " UNION (SELECT * FROM tweets AS T WHERE T.user_id=?)" +
+                                    " ORDER BY created_at DESC RETURNING T.id, T.body," +
+                                    " T.user_id, T.created_at");
+            } catch (SQLException e) {
+                // e.printStackTrace();
             }
         }
     }
