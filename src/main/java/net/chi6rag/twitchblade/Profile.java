16f0f80695e38e7dfe2fe48b82bb9cd3a58aa786
Chirag Aggarwal <me@chi6rag.net>
2015-10-15 18:29:02 +0530

    Profile: Adding code for Profile class & #getTweets method

---
+++ src/main/java/net/chi6rag/twitchblade/Profile.java
@@ -0,0 +1,22 @@
+package net.chi6rag.twitchblade;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class Profile {
+    User user;
+    Tweets allTweets;
+
+    public Profile(User user, DbConnection connection){
+        this.user = user;
+        this.allTweets = new Tweets(connection);
+    }
+
+    public ArrayList<Tweet> getTweets(){
+        if(this.user.getId() == null) { return null; }
+        Hashtable query = new Hashtable();
+        query.put("userId", this.user.getId());
+        return allTweets.where(query);
+    }
+
+}
